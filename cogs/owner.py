24c8874dfb2770e9ef2ee6d22c6f78bb883d18a5
Walter Raja <85942041+Mr-Specs-25@users.noreply.github.com>
2022-06-21 00:15:17 +0530

    updated

--- cogs/owner.py
+++ cogs/owner.py
@@ -15,9 +15,9 @@ class Owner(commands.Cog):
         await ctx.message.delete()
         self.client.load_extension(f"cogs.{extension}")
         channel = self.client.get_channel(988487504812441610)
-        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Successfully loaded {extension}```", timestamp = ctx.message.created_at)
+        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Successfully loaded \"{extension}.py\"```", timestamp = ctx.message.created_at)
         embed.set_author(name = "Extension loaded", icon_url = self.client.user.avatar_url)
-        await ctx.send(f"```py\n Successfully loaded {extension}```", delete_after = 5.0)
+        await ctx.send(f"```py\n Successfully loaded \"{extension}.py\"```", delete_after = 5.0)
         await channel.send(embed = embed)
 
 #unload-extension
@@ -27,9 +27,9 @@ class Owner(commands.Cog):
         await ctx.message.delete()
         self.client.unload_extension(f"cogs.{extension}")
         channel = self.client.get_channel(988487504812441610)
-        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Successfully unloaded {extension}```", timestamp = ctx.message.created_at)
+        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Successfully unloaded \"{extension}.py\"```", timestamp = ctx.message.created_at)
         embed.set_author(name = "Extension unloaded", icon_url = self.client.user.avatar_url)
-        await ctx.send(f"```py\n Successfully unloaded {extension}```", delete_after = 5.0)
+        await ctx.send(f"```py\n Successfully unloaded \"{extension}.py\"```", delete_after = 5.0)
         await channel.send(embed = embed)
 
 #reload_extension
@@ -40,30 +40,9 @@ class Owner(commands.Cog):
         self.client.unload_extension(f"cogs.{extension}")
         self.client.load_extension(f"cogs.{extension}")
         channel = self.client.get_channel(988487504812441610)
-        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Successfully reloaded {extension}```", timestamp = ctx.message.created_at)
+        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Successfully reloaded \"{extension}.py\"```", timestamp = ctx.message.created_at)
         embed.set_author(name = "Extension reloaded", icon_url = self.client.user.avatar_url)
-        await ctx.send(f"```py\n Successfully reloaded {extension}```", delete_after = 5.0)
-        await channel.send(embed = embed)
-
-
-#clear-dms
-    @commands.command()
-    @commands.is_owner()
-    async def cleardms(self, ctx, limit: int = None):
-        await ctx.message.delete()
-        passed = 0
-        failed = 0
-        async for message in ctx.message.channel.history(limit = limit):
-            if message.author.id == self.client.user.id:
-                try:
-                    await message.delete()
-                    passed += 1
-                except:
-                    failed += 1
-        channel = self.client.get_channel(988487504812441610)
-        embed = discord.Embed(color = 0xf9ffff, description = f"```py\n Removed {passed} messages with {failed} fails```", timestamp = ctx.message.created_at)
-        embed.set_author(name = "DMs cleared", icon_url = self.client.user.avatar_url)
-        await ctx.send(f"```py\n Removed {passed} messages with {failed} fails```", delete_after = 5.0)
+        await ctx.send(f"```py\n Successfully reloaded \"{extension}.py\"```", delete_after = 5.0)
         await channel.send(embed = embed)
 
 
